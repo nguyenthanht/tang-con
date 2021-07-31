62b5e1787fa0293acc58a9f772979014d7826428
nguyenthanht <86815188+nguyenthanht@users.noreply.github.com>
2021-07-31 14:35:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hiếu à!',
+    introTitle: 'crush à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'thử không chọn đi cũng lên ăn ké <33',
-    btnNo: 'Không, bạn à never chọn được nhé :3',
+    btnYes: 'không chọn là ký do cái đầu ngu <33',
+    btnNo: ' never  được nhé :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h30 lên phú sơn ăn ké ok.',
+    messDesc: 'Tối nay 7h30 hẹn ok.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100026872742766' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
